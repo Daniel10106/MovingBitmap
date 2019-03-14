636da8fa38f6d7aec539ad25ccdd7114c44a996a
Daniel10106 <dani.10103m@gmail.com>
2019-03-14 21:27:41 +0200

    problems going on with this

--- MovingBitmap/MainPage.xaml.cs
+++ MovingBitmap/MainPage.xaml.cs
@@ -12,6 +12,12 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+using Microsoft.Graphics.Canvas.UI;
+using System.Threading.Tasks;
+using Microsoft.Graphics.Canvas;
+using Windows.UI;
+using Windows.UI.Core;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,17 +28,52 @@ namespace MovingBitmap
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        CanvasBitmap bitmapOrb;
+        float xPos = 0.0f;
+        float yPos = 0.0f;
+
         public MainPage()
         {
             this.InitializeComponent();
+            Window.Current.CoreWindow.KeyDown += ground_KeyDown;
+            Windows.UI.Xaml.Media.CompositionTarget.Rendering += CompositionTarget_Rendering;
         }
 
-        private void Grid_KeyDown(object sender, KeyRoutedEventArgs e)
+        private void Ground_CreateResources(CanvasControl sender, CanvasCreateResourcesEventArgs args)
         {
-            if (e.Key == Windows.System.VirtualKey.A)
+            args.TrackAsyncAction(Ground_CreateResourcesAsync(sender).AsAsyncAction());
+        }
+
+        async Task Ground_CreateResourcesAsync(CanvasControl sender)
+        {
+            bitmapOrb = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/objects/orb.png"));
+        }
+
+        private void Ground_Draw(CanvasControl sender, CanvasDrawEventArgs args)
+        {
+            args.DrawingSession.DrawImage(bitmapOrb, xPos, yPos);
+
+            ground.Invalidate();
+        }
+
+        private void ground_KeyDown(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs args)
+        {
+            if (args.VirtualKey == Windows.System.VirtualKey.A)
             {
-                
-            }
+                xPos += 20.0f;
+            }          
+        }
+
+        private void CompositionTarget_Rendering(object sender, object e)
+        {
+            yPos = (float)bitmapOrb.GetValue(Canvas.TopProperty);
+            xPos = (float)bitmapOrb.GetValue(Canvas.LeftProperty);
+            moveOrb();
+        }
+
+        private void moveOrb()
+        {
+
         }
     }
 }
